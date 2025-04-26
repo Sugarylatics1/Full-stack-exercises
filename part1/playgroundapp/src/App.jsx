ee8d5608c4c2e9dc4b69181daa999b6b55ab1cf6
Sugarylatics1 <87544734+Sugarylatics1@users.noreply.github.com>
2025-04-26 12:44:46 +0800

    New Example

--- part1/playgroundapp/src/App.jsx
+++ part1/playgroundapp/src/App.jsx
@@ -1,41 +1,23 @@
-
 const Hello = (props) => {
   return (
     <div>
-      <p>Greetings {props.name}, you are now {props.age} years old.</p>
-    </div>
-  )
-}
-
-const Footer = (props) => {
-  return (
-    <div>
-      Greeting app created by <a href={props.name}>Jimmir Miranda</a>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
     </div>
   )
 }
 
 const App = () => {
-  const github = 'https://github.com/Sugarylatics1'
-  const nowDate = new Date()
-  const a = 10
-  const b = 20
-  const sum = a + b
-  const name = 'Jimmir'
-  const age = 18
-  const friends = [ {name: 'Peter', age: 4}, {name: 'John', age: 4}, {name: 'Mary', age: 4} ]
+  const name = 'Peter'
+  const age = 10
 
-  console.log('Hello from component')
   return (
-    <>
-      <p>The values of a and b are {a} and {b}</p>
-      <p>The sum of a and b is {sum}</p>
-      <p>And the sum is {a + b}</p>
-      <p>And it is now {nowDate.toString()}</p>
-      <Hello name = {name} age = {age}/>
-      <p>{friends[0].name} {friends[0].age}</p>
-      <Footer name = {github}/>
-    </>
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
   )
 }
 
