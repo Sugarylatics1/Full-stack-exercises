d6f26dd6a6013e5052d30f037b9b26a2ac458504
Sugarylatics1 <87544734+Sugarylatics1@users.noreply.github.com>
2025-04-28 20:19:31 +0800

    Played around with concepts

--- part1/playgroundapp/src/App.jsx
+++ part1/playgroundapp/src/App.jsx
@@ -1,27 +1,45 @@
-const Hello = (props) => {
-  const bornYear = () => {
-    const yearNow = new Date().getFullYear()
-    return yearNow - props.age
-  }
+import {useState} from 'react'
+
+const Hello = ({name, age}) => {
+  //const {name, age} = props
+  const bornYear = () => new Date().getFullYear() - age  
   return (
     <div>
       <p>
-        Hello {props.name}, you are {props.age} years old
+        Hello {name}, you are {age} years old
       </p>
-      <p>So you are probably {bornYear()}</p>
+      <p>So you are probably born in {bornYear()}</p>
     </div>
   )
 }
 
+
 const App = () => {
+  const [ counter, setCounter ] = useState(0)
   const name = 'Peter'
   const age = 10
+  console.log('rendering...', counter)
+  const setToZero = () => setCounter(0)
+  const increaseByOne = () => setCounter(counter + 1)
 
   return (
     <div>
       <h1>Greetings</h1>
       <Hello name="Maya" age={26 + 10} />
       <Hello name={name} age={age} />
+      {counter}
+
+      <p>
+        <button onClick={increaseByOne}>
+          plus
+        </button>
+        <button onClick={setToZero}>
+          zero
+        </button>
+      </p>
+      
+        
+      
     </div>
   )
 }
