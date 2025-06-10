e06fb3836780606f5057661e4f64d6faa53d4bc8
Sugarylatics1 <87544734+Sugarylatics1@users.noreply.github.com>
2025-06-10 12:14:40 +0800

    Added functionalities

--- playgroundapp/src/App.jsx
+++ playgroundapp/src/App.jsx
@@ -75,34 +75,32 @@ export default App
 */
 
 const App = () => {
-  // set constants for the buttons 
-  const [clicks, setClicks] = useState({
-    good: 0, 
-    neutral: 0, 
-    bad: 0
-  })
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
   const [allClicks, setAll] = useState([])
-  const handleGoodClick = () =>{
-    setAll(allClicks.concat('good'))
-    setClicks({ ...clicks, good: clicks.good + 1 })
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+    setTotal(left + right)
   }
-  const handleNeutralClick = () => {
-    setClicks({ ...clicks, neutral: clicks.neutral + 1 })
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+    setTotal(left + right)
   }
-  const handleBadClick = () => {
-    setClicks({ ...clicks, bad: clicks.bad + 1 })
-  }
+
   return (
-    <>
-      <h1>Give Feedback</h1>
-      <button onClick={handleGoodClick}>Good</button>
-      <button onClick={handleNeutralClick}>Neutral</button>
-      <button onClick={handleBadClick}>Bad</button>
-      <h1>Statistics</h1>
-      <p>Good: {clicks.good}</p>
-      <p>Neutral: {clicks.neutral}</p>
-      <p>Bad: {clicks.bad}</p>
-    </>
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
+      <p>total {total}</p>
+    </div>
   )
 }
 
