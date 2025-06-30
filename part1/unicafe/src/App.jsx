52beb965daed40323afa4215aba42dfd98d1b72e
Jimmir Miranda <87544734+Sugarylatics1@users.noreply.github.com>
2025-07-01 01:18:51 +0800

    added stuff

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,30 +1,69 @@
 import { useState } from 'react'
 
+const Statistics = ({good, neutral, bad}) => {
+  if (good + neutral + bad == 0 ){
+    return (
+      <>
+      <p>No feedback given</p>
+      </>
+    )
+  }
+  return (
+    <>
+      <StatisticLine name={"Good"} value={good}/>
+      <StatisticLine name={"Neutral"} value={neutral}/>
+      <StatisticLine name={"Bad"} value={bad}/>
+      <StatisticLine name={"All"} value={good + neutral + bad}/>
+      <StatisticLine name={"Average"} value={(good + (bad * -1))/(good + neutral + bad)}/>
+      <StatisticLine name={"Positive"} value={(good * 100)/(good + neutral + bad)}/>
+    </>
+
+  )
+}
+
+const Button = ({goodClick, badClick, neutralClick}) => {
+  
+  return (
+    <>
+    <ButtonClick onClickValue={goodClick} text={'Good'}/>
+    <ButtonClick onClickValue={neutralClick} text={'Neutral'}/>
+    <ButtonClick onClickValue={badClick} text={'Bad'}/>
+    </>
+  )
+}
+
+const StatisticLine = ({name, value}) => {
+    return (
+      <>
+        <p>{name}: {value}</p>
+      </>
+    )
+}
+
+const ButtonClick = ({onClickValue, text}) => {
+  return (
+    <>
+    <button onClick={onClickValue}>{text}</button>
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
+  // make statistics list with only text and value
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const handleGoodClick = () =>{
-    setGood(good + 1)
-  }
-  const handleNeutralClick = () => {
-    setNeutral(neutral + 1)
-  }
-  const handleBadClick = () => {
-    setBad(bad + 1)
-  }
+  const setGoodClick = () => {setGood(good + 1)}
+  const setNeutralClick = () => {setNeutral(neutral + 1)}
+  const setBadClick = () => {setBad(bad + 1)}
+  
   return (
     <div>
       <h1>Give Feedback</h1>
-      <button onClick={handleGoodClick}>Good</button>
-      <button onClick={handleNeutralClick}>Neutral</button>
-      <button onClick={handleBadClick}>Bad</button>
+      <Button goodClick={setGoodClick} neutralClick={setNeutralClick} badClick={setBadClick}/>
       <h1>Statistics</h1>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
--- playgroundapp/src/App.jsx
+++ playgroundapp/src/App.jsx
@@ -73,6 +73,22 @@ const App = () => {
 export default App
 
 */
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
   const [left, setLeft] = useState(0)
@@ -82,23 +98,25 @@ const App = () => {
 
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-    setTotal(left + right)
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
   }
 
   const handleRightClick = () => {
     setAll(allClicks.concat('R'))
-    setRight(right + 1)
-    setTotal(left + right)
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    setTotal(left + updatedRight)
   }
-
+  
   return (
     <div>
       {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
+      <Button onClick={handleLeftClick} text='left'/>
+      <Button onClick={handleRightClick} text='right'/>
       {right}
-      <p>{allClicks.join(' ')}</p>
+      <History allClicks={allClicks}/>
       <p>total {total}</p>
     </div>
   )
